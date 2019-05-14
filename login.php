9afe645e38503f1a0787b2d4e6ea7d0ab8172946
julia <abuziarovajulia@gmail.com>
2019-05-14 13:09:44 +0200

    14.05

--- login.php
+++ login.php
@@ -1,5 +1,20 @@
 <?php
-$login = $_POST['login'];
-$haslo = $_HASLO['haslo'];
+ require_once "./connect.php";
+
+ $polaczenie = @new mysqli($host,$db_user,$db_password,$db_name);
+
+ if($polaczenie->connect_errno!=0)
+ {
+   echo "Error: ".$polaczenie->connect_errno ."Opis: ".$polaczenie->connect_error;
+ }
+else {
+ $login = $_POST['login'];
+  $haslo = $_HASLO['haslo'];
+
+  echo "It works";
+
+
+$polaczenie->close();
+}
 
  ?>
