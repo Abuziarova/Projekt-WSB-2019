c18dc218ec2a21fb424f6d7b652dd0795abcb6e9
julia <abuziarovajulia@gmail.com>
2019-05-15 13:10:09 +0200

    15.05

--- login.php
+++ login.php
@@ -5,13 +5,30 @@
 
  if($polaczenie->connect_errno!=0)
  {
-   echo "Error: ".$polaczenie->connect_errno ."Opis: ".$polaczenie->connect_error;
+   echo "Error: ".$polaczenie->connect_errno;
  }
 else {
  $login = $_POST['login'];
-  $haslo = $_HASLO['haslo'];
+ $haslo = $_POST['haslo'];
 
-  echo "It works";
+  $sql = "select * from uzytkownicy where login = '$login' and haslo='$haslo'";
+
+  if($rezultat=@$polaczenie->query($sql))
+  {
+    $ilu_userow=$rezultat->num_rows;
+    if($ilu_userow>0)
+    {
+    $wiersz=$rezultat->fetch_assoc();
+    $user=$wiersz['login'];
+
+    $rezultat->free_result();
+echo "zalogowałes się";
+    echo $user;
+    }
+    else {
+    echo "nie udało sie :(";
+    }
+  }
 
 
 $polaczenie->close();
