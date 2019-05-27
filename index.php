3dfd2ec29a259684423f1916b1201e7a49350d8b
julia <abuziarovajulia@gmail.com>
2019-05-27 13:25:17 +0200

    27.05

--- index.php
+++ index.php
@@ -12,14 +12,15 @@
     <form class="" action="./login.php" method="post">
       <p>Login:</p><input type="text" name="login" >
       <p>Password:</p><input type="password" name="haslo" ><br><br>
+      <?php
+   if(isset($_SESSION['blad']))
+      echo $_SESSION['blad']; ?><br>
       <input type="submit" name="" value="Sign in">
       <hr>
       <input type="submit" name="" value="Sign up">
    </form>
 
-   <?php
-if(isset($_SESSION['blad']))
-   echo $_SESSION['blad']; ?>
+
     <h1>English for IT</h1>
 
   </body>
